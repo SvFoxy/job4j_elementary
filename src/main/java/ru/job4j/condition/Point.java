5afe46d89b1f95d1047a3c2108824c197a86bc7b
SvFoxy <sveta_4_you_@mail.ru>
2023-10-12 13:37:17 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,29 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rsl;
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+   public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-
-        double result2 = Point.distance(2, 10, 4, 20);
-        System.out.println("result2 (2, 10) to (4, 20) " + result2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,44 +7,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when040to250then1019() {
         double expected = 10.19;
-        int x1 = 0;
-        int y1 = 40;
-        int x2 = 2;
-        int y2 = 50;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 40);
+        Point b = new Point(2, 50);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when300to21then2801() {
         double expected = 28.01;
-        int x1 = 30;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(30,0);
+        Point b = new Point(2,1);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00to220then2009() {
         double expected = 20.09;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 20;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0,0);
+        Point b = new Point(2,20);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
